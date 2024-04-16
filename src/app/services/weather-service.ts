db8eff39a801e4f1e1666c45fe5c83ffe8bef521
Juan Feliciano <juanfeliciasantos@gmail.com>
2024-04-16 15:58:54 -0300

    feat: added the features from API

---
+++ src/app/services/weather-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  weatherData: any;
+
+  constructor(private http: HttpClient) {}
+
+  fetchWeatherData() {
+    return this.http
+      .get<any>(
+        'https://api.hgbrasil.com/weather?format=json-cors&key=cabab5a1'
+      )
+      .subscribe((data) => {
+        this.weatherData = data;
+      });
+  }
+}
